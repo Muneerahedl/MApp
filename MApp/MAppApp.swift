63140f224b48bdc3648d59aeae5dc4cda8620469
Muneerahedl <muneerah.aledaily@gmail.com>
2022-10-27 16:19:11 +0300

    Initial Commit

---
+++ MApp/MAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAppApp.swift
+//  MApp
+//
+//  Created by Muneerah Aledaily on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
